41b51c801e1494b437994df0dab188a78d8572d2
Loudcow67 <logan.soulliere@ontariotechu.net>
2020-10-28 12:34:19 -0400

    Object Pooling

---
+++ Mail Pilot/Assets/_Scripts/BulletPoolManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: Bonus - make this class a Singleton!
+
+[System.Serializable]
+public class BulletPoolManager : MonoBehaviour
+{
+    public GameObject bullet;
+
+    //TODO: create a structure to contain a collection of bullets
+    public static BulletPoolManager sharedInstance;
+    private List<GameObject> bulletPool;
+    public int poolAmount;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        sharedInstance = this;
+    }
+
+    void Start()
+    {
+        // TODO: add a series of bullets to the Bullet Pool
+        bulletPool = new List<GameObject>();
+        for (int i = 0; i < poolAmount; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(bullet);
+            obj.SetActive(false);
+            bulletPool.Add(obj);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //TODO: modify this function to return a bullet from the Pool
+    public GameObject GetBullet()
+    {
+        for (int i = 0; i < bulletPool.Count; i++)
+        {
+            if (!bulletPool[i].activeInHierarchy)
+            {
+                return bulletPool[i];
+            }
+        }
+        return null;
+    }
+
+    //TODO: modify this function to reset/return a bullet back to the Pool 
+    public void ResetBullet(GameObject bullet)
+    {
+        bullet.SetActive(false);
+    }
+}
